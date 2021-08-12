3d7aecc42c558f4c7a5e18a43a8904ab4a8897a7
Pontus Rohden <pontus.rohden@se.experis.com>
2021-08-12 15:46:21 +0200

    commented questionBox

--- src/components/questionComponents/QuestionBox.vue
+++ src/components/questionComponents/QuestionBox.vue
@@ -30,12 +30,17 @@ export default {
     numberOfQuestions: Number,
   },
   methods: {
+    /**
+     * Adds both correct and incorrect answers to an array, then returns the array shuffled.
+     */
     answers(question) {
       let alternatives = [...question.incorrect_answers];
       alternatives.push(question.correct_answer);
       return this.shuffleAnswers(alternatives);
     },
-    // Durstenfeld shuffle
+    /**
+     * Shuffles an array using Durstenfeld's shuffle
+     */
     shuffleAnswers(answers) {
       for (var i = answers.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -45,6 +50,9 @@ export default {
       }
       return answers;
     },
+    /**
+    * Notifies the parent that an alternative-button has been pressed, and sends back the chosen alternative. 
+    */
     next(alternative) {
       this.$emit("next", alternative);
     },
